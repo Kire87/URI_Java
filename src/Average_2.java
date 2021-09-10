dcf0d7719b8e1b0de83c05c1bdbd9f9a8278f923
Kire87 <erikff7@gmail.com>
2021-09-10 16:25:04 -0300

    Closing the scanner on 1005 - Average 1 and creating 1006 - Average 2

--- src/Average_2.java
+++ src/Average_2.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class Average_1 {
+public class Average_2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		double A = sc.nextDouble();
 		double B = sc.nextDouble();
+		double C = sc.nextDouble();
 		
-		double result = ((A * 3.5) + (B * 7.5)) / 11;
+		double result = ((A * 2) + (B * 3) + (C * 5)) / 10;
 		
-		System.out.printf("MEDIA = %.5f\n", result);
+		System.out.printf("MEDIA = %.1f\n", result);
 		
 		sc.close();
 	}
